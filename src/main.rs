3702c13b5a8192a3846453dce49f44a68a869bc2
Josué Cortina <joshuacs.main@gmail.com>
2024-11-12 17:19:56 +0100

    refactor: Split main function logic

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,43 @@
 use rand::{thread_rng, Rng};
 use std::io::{stdin, stdout, Write};
 
-fn main() {
-    let (stdin, mut stdout) = (stdin(), stdout());
-
-    println!("Welcome to Rust Guessing Game!\nMade with ❤️ by JoshuaCS!\n");
-
-    let mut input = String::new();
-    let mut tries = 0;
-
-    let mut rand = thread_rng();
-
-    println!("The game consists of guessing a randomly selected number\n");
-
-    print!("Let's start by selecting the upper limit: ");
-    stdout.flush().unwrap();
-
-    let upper = loop {
+fn get_upper_limit(input: &mut String) -> u8 {
+    loop {
         input.clear();
 
-        if let Err(e) = stdin.read_line(&mut input) {
+        if let Err(e) = stdin().read_line(input) {
             println!("Something went wrong! {e}");
             print!("Try again: ");
-            stdout.flush().unwrap();
+            stdout().flush().unwrap();
 
             continue;
         }
 
         let input = input.trim();
 
-        let Ok(upper) = input.parse::<u8>() else {
-            println!("'{input}' is not a number!");
-            print!("Try again: ");
-            stdout.flush().unwrap();
-
-            continue;
+        match input.parse::<u8>() {
+            Ok(n) if n > 0 => break n,
+            _ => println!("'{input}' is not valid!"),
         };
 
-        break upper;
-    };
+        print!("Try again: ");
+        stdout().flush().unwrap();
+    }
+}
 
-    let r = rand.gen_range(0..upper);
+fn start(input: &mut String, upper_limit: u8) {
+    let r = thread_rng().gen_range(0..upper_limit);
+    let mut tries = 0;
 
     loop {
         input.clear();
 
         tries += 1;
 
-        print!("Please enter your guess [0-{upper}]: ");
-        stdout.flush().unwrap();
+        print!("Please enter your guess [0-{upper_limit}]: ");
+        stdout().flush().unwrap();
 
-        stdin.read_line(&mut input).unwrap();
+        stdin().read_line(input).unwrap();
 
         let Ok(input) = input.trim().parse::<u8>() else {
             println!("Not a number! Try again!");
@@ -64,4 +51,17 @@ fn main() {
             println!("Wrong number! Try again!");
         }
     }
+}
+
+fn main() {
+    println!("Welcome to Rust Guessing Game!\nMade with ❤️ by JoshuaCS!\n");
+    println!("The game consists of guessing a randomly selected number\n");
+    print!("Let's start by selecting the upper limit: ");
+    stdout().flush().unwrap();
+
+    let input = &mut String::new();
+
+    let upper_limit = get_upper_limit(input);
+
+    start(input, upper_limit);
 }
